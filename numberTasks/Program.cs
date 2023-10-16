eadb75c6ee55d2b301c727e93dbb735b02249b36
undefinedCudaCore <61139751+undefinedCudaCore@users.noreply.github.com>
2023-10-16 19:05:56 +0300

    Add project files.

---
+++ numberTasks/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Security.Claims;
+
+namespace numberTasks // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void CalculateDigits()
+        {
+            Console.WriteLine("Enter number:");
+
+            string cachedNumberString = Console.ReadLine();
+
+            int cachedNumberStringLength = cachedNumberString.Length;
+
+            int cachedNumber = Convert.ToInt32(cachedNumberString);
+
+            //if (cachedNumberStringLength == 0)
+            //{
+            //    CalculateDigits();
+            //}
+            //else
+            if (cachedNumberStringLength == 2)
+            {
+                int firstNumber = cachedNumber / 10;
+                int secondNumber = cachedNumber % 10;
+                int thirdNumber = firstNumber + secondNumber;
+
+                Console.WriteLine(thirdNumber);
+            }
+            else if (cachedNumberStringLength > 2)
+            {
+                int firstNumber = cachedNumber / 100;
+                Console.WriteLine(firstNumber);
+                int secondNumber = cachedNumber / 10 % 10;
+                Console.WriteLine(secondNumber);
+                int thirdNumber = cachedNumber % 10;
+                Console.WriteLine(thirdNumber);
+                int fourthNumber = firstNumber + secondNumber + thirdNumber;
+
+                Console.WriteLine(fourthNumber);
+
+            }
+            else if (cachedNumberStringLength > 3)
+            {
+                CalculateDigits();
+            }
+            CalculateDigits();
+        }
+
+        static void RepeatCalculateDigits()
+        {
+
+        }
+        static void Main(string[] args)
+        {
+
+            //Console.WriteLine("Enter number:");
+
+            //string cachedNumberString = Console.ReadLine();
+
+            //int cachedNumber = Convert.ToInt32(cachedNumberString);
+
+            //bool test = false;
+            //if (test)
+            //{
+
+            //}
+            //for (int i = 0; i < length; i++)
+            //{
+
+            //}
+            //if (cachedNumber != 0 || cachedNumber == 2)
+            //{
+            //    int firstNumber = cachedNumber / 10;
+            //    int secondNumber = cachedNumber % 10;
+            //    int thirdNumber = firstNumber + secondNumber;
+
+            //    Console.WriteLine(thirdNumber);
+            //}
+            //else
+            //{
+            //    CalculateDigits();
+            //}
+
+
+            //Console.ReadKey();
+            CalculateDigits();
+
+        }
+    }
+}
