48fdf4cadb0d2bf16cee64435a05f7ee6e18f58f
undefinedCudaCore <61139751+undefinedCudaCore@users.noreply.github.com>
2023-10-18 19:33:39 +0300

    Add task No one and task No two

---
+++ TasksForConditionalSentences/Program.cs
@@ -0,0 +1,40 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Console.WriteLine("Task No. 1");
+        //Console.WriteLine();
+
+        //Console.WriteLine("Enter first number:");
+        //var getA = Console.ReadLine();
+
+        //Console.WriteLine("Enter second number:");
+        //var getB = Console.ReadLine();
+
+        //if (getA == getB)
+        //{
+        //    Console.WriteLine($"First number {getA} equals to second number {getB}.");
+        //} else
+        //{
+        //    Console.WriteLine($"The first and second numbers do not equal each other.");
+        //}
+
+        //Console.WriteLine("Task No. 2");
+        //Console.WriteLine();
+
+        //Console.WriteLine("Enter number:");
+        //var getNumberThree = Console.ReadLine();
+
+        //int getNumberThreeOne = Convert.ToInt32(getNumberThree) / 1000;
+        //Console.WriteLine(getNumberThreeOne);
+
+        //if ((getNumberThreeOne % 2) == 0)
+        //{
+        //    Console.WriteLine($"The digit {getNumberThreeOne} of the number {getNumberThree} is even.");
+        //}
+        //else
+        //{
+        //    Console.WriteLine($"The digit {getNumberThreeOne} of the number {getNumberThree} is odd.");
+        //}
+    }
+}
